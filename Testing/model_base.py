c02775e620a73ae1df5507ea5349aad0c088758a
Wilson92-isep <antoine.wilson12@gmail.com>
2024-02-28 12:14:48 +0100

    init

---
+++ Testing/model_base.py
@@ -0,0 +1,154 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import TensorDataset, DataLoader
+import pandas as pd
+import numpy as np
+
+# Activities are the class labels
+# It is a 6 class classification
+ACTIVITIES = {
+    0: 'WALKING',
+    1: 'WALKING_UPSTAIRS',
+    2: 'WALKING_DOWNSTAIRS',
+    3: 'SITTING',
+    4: 'STANDING',
+    5: 'LAYING',
+}
+
+# Utility function to print the confusion matrix
+def confusion_matrix(Y_true, Y_pred):
+    Y_true = pd.Series([ACTIVITIES[y] for y in np.argmax(Y_true, axis=1)])
+    Y_pred = pd.Series([ACTIVITIES[y] for y in np.argmax(Y_pred, axis=1)])
+
+    return pd.crosstab(Y_true, Y_pred, rownames=['True'], colnames=['Pred'])
+
+# Data directory
+DATADIR = 'UCI_HAR_Dataset'
+
+# Raw data signals
+# Signals are from Accelerometer and Gyroscope
+# The signals are in x,y,z directions
+# Sensor signals are filtered to have only body acceleration
+# excluding the acceleration due to gravity
+# Triaxial acceleration from the accelerometer is total acceleration
+SIGNALS = [
+    "body_acc_x",
+    "body_acc_y",
+    "body_acc_z",
+    "body_gyro_x",
+    "body_gyro_y",
+    "body_gyro_z",
+    "total_acc_x",
+    "total_acc_y",
+    "total_acc_z"
+]
+
+# Utility function to read the data from csv file
+def _read_csv(filename):
+    return pd.read_csv(filename, delim_whitespace=True, header=None)
+
+# Utility function to load the load
+def load_signals(subset):
+    signals_data = []
+
+    for signal in SIGNALS:
+        filename = f'../UCI_HAR_Dataset/{subset}/Inertial Signals/{signal}_{subset}.txt'
+        signals_data.append(
+            _read_csv(filename).to_numpy()
+        ) 
+
+    # Transpose is used to change the dimensionality of the output,
+    # aggregating the signals by combination of sample/timestep.
+    # Resultant shape is (7352 train/2947 test samples, 128 timesteps, 9 signals)
+    return np.transpose(signals_data, (1, 2, 0))
+
+
+def load_y(subset):
+    """
+    The objective that we are trying to predict is an integer, from 1 to 6,
+    that represents a human activity. We return a binary representation of 
+    every sample objective as a 6 bits vector using One Hot Encoding
+    """
+    filename = f'../UCI_HAR_Dataset/{subset}/y_{subset}.txt'
+    y = _read_csv(filename)[0]
+
+    return pd.get_dummies(y).to_numpy()
+
+
+def load_data():
+    """
+    Obtain the dataset from multiple files.
+    Returns: X_train, X_test, y_train, y_test
+    """
+    X_train, X_test = load_signals('train'), load_signals('test')
+    y_train, y_test = load_y('train'), load_y('test')
+
+    return X_train, X_test, y_train, y_test
+
+
+# Setting seed for reproducibility
+np.random.seed(42)
+torch.manual_seed(42)
+
+# Loading the train and test data
+X_train, X_test, Y_train, Y_test = load_data()
+
+# Converting data to PyTorch tensors
+X_train_tensor = torch.tensor(X_train, dtype=torch.float32)
+Y_train_tensor = torch.tensor(Y_train, dtype=torch.float32)
+X_test_tensor = torch.tensor(X_test, dtype=torch.float32)
+Y_test_tensor = torch.tensor(Y_test, dtype=torch.float32)
+
+# Initializing parameters
+epochs = 30
+batch_size = 32
+n_hidden = 128
+pv = 0.25
+
+# Define LSTM model
+class LSTMModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(LSTMModel, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
+        self.batch_norm = nn.BatchNorm1d(hidden_size)
+        self.dropout = nn.Dropout(pv)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = self.batch_norm(out[:, -1, :])
+        out = self.dropout(out)
+        out = self.fc(out)
+        return out
+
+# Create model
+model = LSTMModel(input_size=X_train_tensor.size(2), hidden_size=n_hidden, output_size=Y_train_tensor.size(1))
+print(model)
+
+# Loss and optimizer
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.RMSprop(model.parameters())
+
+# Train the model
+for epoch in range(epochs):
+    model.train()
+    optimizer.zero_grad()
+    outputs = model(X_train_tensor)
+    loss = criterion(outputs, Y_train_tensor)
+    loss.backward()
+    optimizer.step()
+    print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
+
+# Evaluate the model
+model.eval()
+with torch.no_grad():
+    outputs = model(X_test_tensor)
+    predicted = torch.sigmoid(outputs)
+    predicted_class = torch.argmax(predicted, dim=1)
+
+# Confusion Matrix
+Y_test_np = np.argmax(Y_test, axis=1)
+predicted_class_np = predicted_class.numpy()
+conf_matrix = confusion_matrix(Y_test_np, predicted_class_np)
+print(conf_matrix)
